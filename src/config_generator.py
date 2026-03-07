f4fb4c2cae2aa289151ebe768a95ee775dacccf4
sadiiiqqqqqq <mohdsadiq7635@gmail.com>
2026-03-07 21:34:58 +0530

    generator halfway

--- src/config_generator.py
+++ src/config_generator.py
@@ -1,1 +1,23 @@
 
+import input_handler as imp
+def collect_user_input():
+
+    config = {}
+    config["hostname"] =imp.ask_non_empty("Enter Hostname :")
+    config["interface"]= []
+    while imp.ask_yes_or_no("Do you want to configure a interface :"):
+        iface = {}
+        iface["name"]= imp.ask_non_empty("Enter name of interfcae (g0/0):")
+        iface["dhcp"]= imp.ask_yes_or_no("Do you want to configure dhcp :")
+        if not iface["dhcp"]:
+            iface["ip"] =imp.ask_non_empty("Enter IP address :")
+            iface["mask"]=imp.ask_non_empty("Enter Subnet mask:")
+        config["interface"].append[iface]
+    config["ssh"] = []
+    while imp.ask_yes_or_no("Do you want to configure SSh"):
+        issh = {}
+        issh["local"]= imp.ask_non_empty("Enter Local username :")
+        issh["localpass"] = imp.ask_non_empty("Enter local username password")
+        issh["domain"] = imp.ask_non_empty("Enter ")
+    
+      
